c03bd1fdb980d0110094edd27527c4a372f41f5f
Igor <igorlfs@ufmg.br>
2023-12-11 15:02:14 -0300

    design: remove this "Hero" stuff cause I don't what to put in there

--- src/routes/(waves)/+page.svelte
+++ src/routes/(waves)/+page.svelte
@@ -1,5 +1,4 @@
 <script lang="ts">
-	import Hero from '$lib/components/organisms/Hero.svelte';
 	import About from '$lib/components/organisms/About.svelte';
 	import RecentPosts from '$lib/components/organisms/RecentPosts.svelte';
 	import Features from '$lib/components/organisms/Features.svelte';
@@ -14,7 +13,6 @@
 </script>
 
 <div class="container">
-	<Hero />
 	<About />
 	{#if posts && posts.length > 0}
 		<RecentPosts {posts} />
