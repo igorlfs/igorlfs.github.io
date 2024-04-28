8d7941a71f0670b59317fe185d2c630cc7d975ce
Igor <igorlfs@ufmg.br>
2024-04-28 15:31:25 -0300

    fixup error page

--- src/routes/+error.svelte
+++ src/routes/+error.svelte
@@ -2,7 +2,7 @@
     import { page } from "$app/stores";
 </script>
 
-<div class="h-full my-10 grid place-content-center">
+<div class="h-full my-10 grid place-content-center text-text">
     <h1 class="text-lavender text-3xl font-bold">{$page.status}</h1>
-    <p class="text-text">{$page.error?.message}</p>
+    <p>{$page.error?.message}</p>
 </div>
