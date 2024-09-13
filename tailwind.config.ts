dfa94ad9bc753b623f0bd8609651ae3c56ef7c3f
Igor <igorlfs@ufmg.br>
2024-09-13 18:35:39 -0300

    slightly improve colors

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,17 +1,28 @@
 import type { Config } from "tailwindcss";
 import typography from "@tailwindcss/typography";
 
+const text = "#cdd6f4";
+const subtext = "#a6adc8";
+
+const lavender = "#b4befe";
+
+const base = "#1e1e2e";
+const mantle = "#181825";
+const crust = "#11111b";
+
+const blue = "#89b4fa";
+
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     colors: {
-      text: "#cdd6f4",
-      subtext: "#a6adc8",
-      lavender: "#b4befe",
-      base: "#1e1e2e",
-      mantle: "#181825",
-      crust: "#11111b",
-      blue: "#89b4fa",
+      text,
+      subtext,
+      lavender,
+      base,
+      mantle,
+      crust,
+      blue,
     },
     extend: {
       fontFamily: {
