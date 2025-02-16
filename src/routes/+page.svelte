deb7da77a2b41761f69dd6b06264f7d64d9ba89c
Igor <igorlfs@ufmg.br>
2025-02-16 16:40:39 -0300

    bump deps

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -11,7 +11,7 @@
 <section class="text-text my-10 flex justify-center">
     <ul class="flex flex-col divide-y-2 divide-crust xl:w-[60%] w-[90%]">
         {#each data.posts as post}
-            <li class="[&:not(:first-child)]:pt-3 pb-3">
+            <li class="not-first:pt-3 pb-3">
                 <a href="{base}/{post.slug}" class="capitalize sm:text-5xl text-3xl">{post.title}</a
                 >
                 <p class="mt-2 text-subtext">{formatDate(post.date)}</p>
